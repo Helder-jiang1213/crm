0c71cb674a38af41071a8be0f3ac009d7dbe569f
Helder_jiang <2499876332@qq.com>
2020-02-26 11:50:25 +0800

    crm 营销机会查询功能实现

---
+++ src/main/java/com/shsxt/crm/dao/SaleChanceMapper.java
@@ -0,0 +1,9 @@
+package com.shsxt.crm.dao;
+
+import com.shsxt.base.BaseMapper;
+import com.shsxt.crm.vo.SaleChance;
+
+
+public interface SaleChanceMapper extends BaseMapper<SaleChance,Integer> {
+
+}
